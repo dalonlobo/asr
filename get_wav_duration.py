3743e3bf8c0596c541a91a44d9014dc89c343da4
Dalon-ubuntu <dalonlobo@gmail.com>
2018-01-16 12:40:54 +0530

    get_wav_duration.py

--- get_wav_duration.py
+++ get_wav_duration.py
@@ -21,7 +21,7 @@ def print_time(secs):
     print("DAYS:HOURS:MIN:SEC")
     print("%d:%d:%d:%d" % (d.day-1, d.hour, d.minute, d.second))
 
-def duration(fpath, above_5s):
+def duration(fpath, all_durations):
     print("Calculating duration: ", fpath)
     total_duration = 0
     print("Total files: ", len(glob.glob(fpath + '/*.wav')))
@@ -31,31 +31,31 @@ def duration(fpath, above_5s):
                 frames = f.getnframes()
                 rate = f.getframerate()
                 file_duration = frames / float(rate)
-                above_5s.append(file_duration)
+                all_durations.append((file_duration, files))
                 total_duration += file_duration
     except:
         pass
-    return (total_duration, above_5s)
+    return (total_duration, all_durations)
 
 if __name__ == "__main__":    
     fpath = sys.argv[1]
     print(fpath)
     duration_s = 0
-    above_5s = []
+    all_durations = []
     total_dirs = len(os.listdir(fpath))
     for index, dirs in enumerate(os.listdir(fpath)):
         directory = os.path.join(fpath, dirs)
         if not os.path.isdir(directory):
             continue # If its not directory, just continue
-        dur, above_5s = duration(os.path.join(directory,"samples"), above_5s)
+        dur, all_durations = duration(os.path.join(directory,"samples"), all_durations)
         duration_s += dur
         print("Completed {} of {}".format(index, total_dirs))
     print()
-    print("Max duration of files: ",max(above_5s))
-    print("Number of files above 5s: ",len(above_5s))
+    print("Max duration of files: ",max(all_durations))
+    print("Number of files above 5s: ",len(all_durations))
     print()
     with open("wav_greater_than_5_"+datetime.now().strftime('%Y-%m-%d-%H-%M')+".b",'wb') as f:
-        pickle.dump(above_5s, f)   
+        pickle.dump(all_durations, f)   
         print("Above 5s list is pickled")
     print("Total duration in hours {}hrs".format((duration_s/60)/60))
     print_time(int(duration_s))
